faaf52ebde89e33a2874a960e72fc6de9b7933a5
m081779 <marcoprincipio@hotmail.com>
2017-12-08 14:40:27 -0500

    small tweaks to client side js

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.post('/login', function (req, res) {
+	console.log(req.body);
+    res.status(200).end();
+});
+
+app.get('/userview', function (req, res) {
+	console.log('get request firing')
+    res.render('userview');
+});
+
+app.post('/api/create', function (req, res) {
+	console.log('new user: ', req.body)
+    res.status(200).end();
+});
+
+app.listen(PORT, ()=>{
+	console.log(`App listening on http://localhost:${PORT}`)
+})
