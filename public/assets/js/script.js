1e5889da23a580567d1c8bdf72db326a17ec22ba
Taliesin <tdcochran2@gmail.com>
2017-12-09 11:38:49 -0500

    added route and config.json

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -14,42 +14,6 @@ $(document).ready(() => {
 		});
 	}
 
-	function checkEmpty() {
-		let args = Array.prototype.slice.call(arguments);
-		let counter = 0;
-		args.map(element => {
-			let empty;
-			if ($(element).is(':radio')) {
-				counter+=0.5
-				empty = $(element).is(':checked') ? false:true;
-			} else {
-				empty = $(element).val()!=='' ? false:true;
-			}
-			if (empty) {
-				showError(element, true);
-				$(element).val('');
-			} else {
-				counter++;
-				showError(element, false);
-			}
-		});
-		console.log('counter & args.length: '+counter+' '+ args.length);
-		console.log(counter)
-		return counter===args.length ? true : false;
-	}
-
-	function showError(element, bool) {
-		let color = ''
-		bool ? color = 'red' : 'white';	
-		if ($(element).is(':radio')) {
-			$(element).siblings('.radio-label').css('color', color)
-		} else {
-			$(element).css('border-color', color);
-		}
-
-	}
-
-
 	//click events for opening and closing modals
 	openModal('sign-in', 'sign-in-modal');
 	openModal('create-account', 'create-account-modal');
