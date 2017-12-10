66f915c3da867befc2358eca1e9d6049cd3a1f20
m081779 <marcoprincipio@hotmail.com>
2017-12-09 21:25:26 -0500

    added validation to check if same password is entered twice

--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -17,15 +17,15 @@ function loginUser() {
 	let user = {}
 	user.userName = $('#username').val().trim();
 	user.password = $('#password').val().trim();
-	user.online = 1;
+	user.online   = 1;
 	clean = checkEmpty('#password', '#username');
 	
 	$.ajax('/login', {
 		type:'POST',
 		data: user
 	}).done((res)=>{
-		if (res==='OK') 
-			console.log('User logged in: ', user){
+		if (res==='OK') {
+			console.log('User logged in: ', user)
 			$('#sign-in-modal').fadeOut();
 			$.get('/userView');
 		}
@@ -33,18 +33,18 @@ function loginUser() {
 }
 
 function createUser() {
-	let clean = false,
-		user = {},
-		rightAge = checkAge('#create-age'),
+	let user = {},
+		rightAge   = checkAge('#create-age'),
 	    cleanInput = checkInputs('#create-username','#create-password', '#create-password2'),
-	    cleanRadio = checkRadio('input[name="gender"]', 'input[name="seeking"]');
+	    cleanRadio = checkRadio('input[name="gender"]', 'input[name="seeking"]'),
+	    samePswd   = checkPasswordsEqual();
 	user.userName = $('#create-username').val().trim();
 	user.password = $('#create-password').val().trim();
-	user.gender = $("input[name='gender']:checked").val();
-	user.seeking = $("input[name='seeking']:checked").val();
-	user.age = $('#create-age').val().trim();
-	user.online = 0;
-	if (rightAge && cleanInput && cleanRadio) {
+	user.gender   = $("input[name='gender']:checked").val();
+	user.seeking  = $("input[name='seeking']:checked").val();
+	user.age      = $('#create-age').val().trim();
+	user.online   = 0;
+	if (rightAge && cleanInput && cleanRadio && samePswd) {
 		
 		$.ajax('/api/create', {
 			type:'POST',
@@ -69,5 +69,5 @@ function clearInputs() {
 
 function userSwipe() {
 	let swipe = $(this).attr('data-swipe'),
-		user = $(this).data('user');
+		user  = $(this).data('user');
 }
