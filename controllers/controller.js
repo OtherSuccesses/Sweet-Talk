40721afa3e7999f59139d62393c591ea7507445e
m081779 <marcoprincipio@hotmail.com>
2017-12-12 20:14:50 -0500

    fixed create user so that on creation login screen opens populated with their data

--- controllers/controller.js
+++ controllers/controller.js
@@ -22,7 +22,6 @@ router.get("/userView", function (req,res) {
       // online: true
     }
   }).then((results)=>{
-    console.log("line 23", results[0].dataValues);
     var users = [];
     for(var i = 0; i<results.length; i++) {
       if(results[i].dataValues.userName !== currentUser.userName) {
@@ -106,7 +105,7 @@ router.post('/api/create', function (req, res) {
       age,
       online
     }).then(function(data) {
-      res.redirect('/userView');
+      res.sendStatus(200);
     });
   })
 });
