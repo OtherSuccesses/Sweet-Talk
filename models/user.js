e7d95d19600ef26ee0d81a86e4746deceba763ed
EJ Morgan <edwardmorgan@EJs-MacBook-Pro.local>
2017-12-20 17:05:50 -0500

    User table fix

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 //Model for user table
 //=====================
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define('User', {
+    var User = sequelize.define('user', {
         userName: {
             type: DataTypes.STRING,
             allowNull: false,
